a3128303e9cbb9f4cc2510629b3ab0077ad00e00
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-06-03 21:46:51 +0200

    added Button and Header to Products component

--- src/components/order/Products.tsx
+++ src/components/order/Products.tsx
@@ -1,17 +1,28 @@
-import { Paper, Stack } from "@mui/material";
+import {
+  Paper,
+  FormGroup,
+  Button,
+  Typography,
+  Stack,
+} from "@mui/material";
 import products from "../../data/products.json";
 import ProductItem from "./ProductItem";
 
 function Products() {
   const productsItems = products.productItems;
   return (
-      <Paper elevation={3} sx={{margin: 5, padding: 5}}>
-        <Stack>
-          {productsItems.map((product) => (
-            <ProductItem key={product.productId} productItem={product} />
-          ))}
-        </Stack>
-      </Paper>
+    <Paper elevation={3} sx={{ margin: 5, padding: 5 }}>
+      <Typography variant="h5" gutterBottom textAlign="center">Place your order</Typography>
+      <FormGroup>
+        {productsItems.map((product) => (
+          <ProductItem key={product.productId} productItem={product} />
+        ))}
+      </FormGroup>
+      <Stack spacing={1} display="flex" alignItems="end">
+        <Typography variant="h6"> 19$</Typography>
+        <Button variant="contained">Order!</Button>
+      </Stack>
+    </Paper>
   );
 }
 
