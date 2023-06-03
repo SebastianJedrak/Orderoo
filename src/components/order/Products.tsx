a739382e85f21a6717cd4d41845133570acee523
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-06-03 21:33:46 +0200

    prepared container for Products

--- src/components/order/Products.tsx
+++ src/components/order/Products.tsx
@@ -5,13 +5,13 @@ import ProductItem from "./ProductItem";
 function Products() {
   const productsItems = products.productItems;
   return (
-    <Paper >
-      <Stack>
-        {productsItems.map((product) => (
-          <ProductItem key={product.productId} productItem={product} />
-        ))}
-      </Stack>
-    </Paper>
+      <Paper elevation={3} sx={{margin: 5, padding: 5}}>
+        <Stack>
+          {productsItems.map((product) => (
+            <ProductItem key={product.productId} productItem={product} />
+          ))}
+        </Stack>
+      </Paper>
   );
 }
 
