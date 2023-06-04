5ea4033cb7542b18844293e1b7f1d7aa3773abe5
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-06-04 11:30:09 +0200

    preparted Accordion component

--- src/components/PackagesAccordion.tsx
+++ src/components/PackagesAccordion.tsx
@@ -1,4 +1,4 @@
-import { Paper } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Paper, Typography } from "@mui/material";
 import { useContext } from "react";
 import { ProductsContext } from "../ctx/ProductsContext";
 
@@ -8,10 +8,23 @@ export default function Packages() {
   const packages = data?.packages;
 
   return (
-    <Paper
-      component="section"
-      elevation={3}
-      sx={{ margin: 5, padding: 5 }}
-    ></Paper>
+    <Paper component="section" elevation={3} sx={{ margin: 5, padding: 5 }}>
+      <Accordion
+        // expanded={expanded === "panel1"}
+        // onChange={handleChange("panel1")}
+      >
+        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+          <Typography>Collapsible Group Item #1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
+            lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </Paper>
   );
 }
