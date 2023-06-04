b79ab24450849f46f936b8c9db64c7b8ffebc14b
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-06-04 06:46:19 +0200

    created label and width for Select

--- src/components/order/Products.tsx
+++ src/components/order/Products.tsx
@@ -6,6 +6,8 @@ import {
   Stack,
   Select,
   MenuItem,
+  InputLabel,
+  FormControl,
 } from "@mui/material";
 import products from "../../data/products.json";
 import ProductItem from "./ProductItem";
@@ -20,26 +22,29 @@ function Products() {
       </Typography>
 
       {/* Select year */}
-      <Select fullWidth label="select country">
-        {Object.keys(productsItems[0].productPrice).map((year) => (
-          <MenuItem key={year} value={year}>
-            {year}
-          </MenuItem>
-        ))}
-      </Select>
+      <FormControl fullWidth>
+        <InputLabel size="small" id="select-year-label">Select year</InputLabel>
+        <Select sx={{width: "150px"}} size="small" labelId="select-year-label" label="Select year" id="select-year">
+          {Object.keys(productsItems[0].productPrice).map((year) => (
+            <MenuItem key={year} value={year}>
+              {year}
+            </MenuItem>
+          ))}
+        </Select>
 
-      {/* Checkbox items*/}
-      <FormGroup>
-        {productsItems.map((product) => (
-          <ProductItem key={product.productId} productItem={product} />
-        ))}
-      </FormGroup>
+        {/* Checkbox items*/}
+        <FormGroup>
+          {productsItems.map((product) => (
+            <ProductItem key={product.productId} productItem={product} />
+          ))}
+        </FormGroup>
 
-      {/* Summary and order*/}
-      <Stack spacing={1} display="flex" alignItems="end">
-        <Typography variant="h6"> 19$</Typography>
-        <Button variant="contained">Order!</Button>
-      </Stack>
+        {/* Summary and order*/}
+        <Stack spacing={1} display="flex" alignItems="end">
+          <Typography variant="h6"> 19$</Typography>
+          <Button variant="contained">Order!</Button>
+        </Stack>
+      </FormControl>
     </Paper>
   );
 }
