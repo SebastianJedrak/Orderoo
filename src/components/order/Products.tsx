7fcbf819a0bf9d27225991a7ec6b7ad3233f3f5a
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-06-04 06:34:27 +0200

    prepared select year for Products component

--- src/components/order/Products.tsx
+++ src/components/order/Products.tsx
@@ -4,6 +4,8 @@ import {
   Button,
   Typography,
   Stack,
+  Select,
+  MenuItem,
 } from "@mui/material";
 import products from "../../data/products.json";
 import ProductItem from "./ProductItem";
@@ -12,12 +14,28 @@ function Products() {
   const productsItems = products.productItems;
   return (
     <Paper component="section" elevation={3} sx={{ margin: 5, padding: 5 }}>
-      <Typography variant="h5" gutterBottom textAlign="center">Place your order</Typography>
+      {/* Header */}
+      <Typography variant="h5" gutterBottom textAlign="center">
+        Place your order
+      </Typography>
+
+      {/* Select year */}
+      <Select fullWidth label="select country">
+        {Object.keys(productsItems[0].productPrice).map((year) => (
+          <MenuItem key={year} value={year}>
+            {year}
+          </MenuItem>
+        ))}
+      </Select>
+
+      {/* Checkbox items*/}
       <FormGroup>
         {productsItems.map((product) => (
           <ProductItem key={product.productId} productItem={product} />
         ))}
       </FormGroup>
+
+      {/* Summary and order*/}
       <Stack spacing={1} display="flex" alignItems="end">
         <Typography variant="h6"> 19$</Typography>
         <Button variant="contained">Order!</Button>
