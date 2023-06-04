67f8e500729e70a929e952fc775bf30316e6d716
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-06-04 11:26:55 +0200

    imported context to PachagesAccordion component

--- src/components/PackagesAccordion.tsx
+++ src/components/PackagesAccordion.tsx
@@ -1,6 +1,12 @@
 import { Paper } from "@mui/material";
+import { useContext } from "react";
+import { ProductsContext } from "../ctx/ProductsContext";
 
 export default function Packages() {
+  const data = useContext(ProductsContext);
+  const productItems = data?.productItems;
+  const packages = data?.packages;
+
   return (
     <Paper
       component="section"
