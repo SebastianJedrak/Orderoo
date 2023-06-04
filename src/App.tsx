19b0c63bd55d5198155771b424073d52ba4b378e
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-06-04 08:58:34 +0200

    wrapped App components in  ProductsProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import "./App.css";
 import Box from "@mui/material/Box/Box";
 import Header from "./components/Header";
 import Products from "./components/order/Products";
-import PackagesAccordion from "./components/PackagesAccordion"
+import PackagesAccordion from "./components/PackagesAccordion";
+import ProductsProvider from "./ctx/ProductsContext";
 
 function App() {
   return (
-    <Box>
-      <Header />
-      <Products />
-      <PackagesAccordion />
-    </Box>
+    <ProductsProvider>
+      <Box>
+        <Header />
+        <Products />
+        <PackagesAccordion />
+      </Box>
+    </ProductsProvider>
   );
 }
 
