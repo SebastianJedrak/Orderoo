b1f6f80abb52238bcd86fd51747af6682d44882a
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-06-14 13:51:50 +0200

    prepared DialogFormSubmit component content

--- src/components/DialogFormSubmit.tsx
+++ src/components/DialogFormSubmit.tsx
@@ -1,5 +1,29 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
+import { useState } from "react";
+
 export default function DialogFormSubmit() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const closeHandler = () => {};
+
   return (
-    <div>DialogFormSubmit</div>
-  )
+    <Dialog open={true} onClose={closeHandler}>
+      <DialogTitle>Successfully Ordered!</DialogTitle>
+      <DialogContent>
+        <DialogContentText>Ordered items and price</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button autoFocus onClick={closeHandler}>
+          Ok
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 }
