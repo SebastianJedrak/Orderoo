c7534a6f6540a07cd33e2f39f02b1bfd57fd3c74
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-06-20 15:11:38 +0200

    prepared layout for Products in AdminView

--- src/components/AdminView/AdminView.tsx
+++ src/components/AdminView/AdminView.tsx
@@ -1,6 +1,57 @@
+import { useContext } from "react";
+import { ProductsContext } from "../../ctx/ProductsContext";
+import {
+  Box,
+  Divider,
+  IconButton,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
+
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export default function AdminView() {
+  const data = useContext(ProductsContext);
+  const productItems = data?.productInSelectedYear;
+  const packageItems = data?.packageInSelectedYear;
+
   return (
-    <div>AdminView</div>
-  )
+    <Box>
+      <Paper
+        component="section"
+        elevation={3}
+        sx={{ mx: "auto", padding: 5, maxWidth: 700 }}
+      >
+        <Typography
+          variant="h5"
+          gutterBottom
+          textAlign="center"
+          color="primary"
+        >
+          Products
+        </Typography>
+        <Stack spacing={1}>
+          {productItems!.map((item) => (
+            <>
+              <Stack direction={"row"} justifyContent={"space-between"} alignItems={"center"}>
+                <Typography key={item.productId}>{item.productName}</Typography>
+                <Box>
+                  <IconButton aria-label="edit" color="primary">
+                    <EditIcon />
+                  </IconButton>
+                  <IconButton aria-label="delete" color="primary">
+                    <DeleteIcon />
+                  </IconButton>
+                </Box>
+              </Stack>
+
+              <Divider />
+            </>
+          ))}
+        </Stack>
+      </Paper>
+    </Box>
+  );
 }
