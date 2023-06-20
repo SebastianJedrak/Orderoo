d26bd20d8fbefb0d4e6a2dabf78c7b9b31595aff
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-06-20 14:45:56 +0200

    added Button to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,27 @@
-import { Typography, Paper } from "@mui/material";
+import { Typography, Paper, Button, Box, Stack } from "@mui/material";
 import { SetStateAction } from "react";
 
 type Props = {
-  view: string,
-  changeView: React.Dispatch<SetStateAction<string>>
-}
+  view: string;
+  changeView: React.Dispatch<SetStateAction<string>>;
+};
 
 function Header(props: Props) {
   return (
-    <Paper elevation={12} component="header">
-      <Typography
-        py={2}
-        textAlign="center"
-        bgcolor="primary.main"
-        color="common.white"
-        variant="h4"
-      >
+    <Stack
+      component="header"
+      direction={"row"}
+      justifyContent={"space-between"}
+      alignItems={"center"}
+      bgcolor="primary.main"
+      px={3}
+    >
+      <Box sx={{width: "11%"}}/>
+      <Typography py={2} textAlign="center" color="common.white" variant="h4">
         Media Connect
       </Typography>
-    </Paper>
+      <Button sx={{width: "11%"}} size="small" variant="contained" color="secondary">Change to Client</Button>
+    </Stack>
   );
 }
 
