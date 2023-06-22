6d0f2fce8ca48be16089b3a94628262c3966fe2d
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-06-22 13:09:12 +0200

    succesfully delete package if contained product is deleted

--- src/components/AdminView/DialogDelete.tsx
+++ src/components/AdminView/DialogDelete.tsx
@@ -33,9 +33,18 @@ export default function DialogFormSubmit(props: Props) {
       const updatedProducts = dataStorage.productItems.filter(
         (item) => item.productId !== props.product?.[0].productId
       );
-      const newData = {productItems: updatedProducts, packages: dataStorage.packages}
-      localStorage.setItem("data", JSON.stringify(newData))
-      setData!(newData)
+      const updatedPackages = dataStorage.packages.filter((item) =>
+        !item.productsIncludedId.some(
+          (productId) => productId === props.product?.[0].productId
+        )
+      );
+      console.log(updatedPackages);
+      const newData = {
+        productItems: updatedProducts,
+        packages: updatedPackages,
+      };
+      localStorage.setItem("data", JSON.stringify(newData));
+      setData!(newData);
     }
     if (props.package !== null) {
     }
