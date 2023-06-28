e207be24e4fe412ac32efdc574fa52907ba24a99
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-06-28 14:10:51 +0200

    created removeYearHandler function

--- src/components/AdminView/FormAddEdit.tsx
+++ src/components/AdminView/FormAddEdit.tsx
@@ -1,6 +1,7 @@
 import { IconButton, Stack, TextField, Typography } from "@mui/material";
 import { YEARS } from "../ClientView/Order/SelectYear";
 import ControlPointIcon from "@mui/icons-material/ControlPoint";
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 import { useState } from "react";
 
 export default function FormAddEdit() {
@@ -10,14 +11,15 @@ export default function FormAddEdit() {
     setYearsArr((prev) => [...prev, prev.at(-1)! + 1]);
   };
 
-  console.log(yearsArr);
+  const removeYearHandler = () => {}
+
   return (
     <form method="POST">
       <Typography>Product Name</Typography>
       <TextField type="text" autoFocus />
 
       <Typography>Prices</Typography>
-      {yearsArr.map((el, i) => (
+      {yearsArr.map((el, i, arr) => (
         <Stack key={i} direction={"row"}>
           <TextField
             label="Year"
@@ -30,6 +32,9 @@ export default function FormAddEdit() {
             type="number"
             InputProps={{ endAdornment: "PLN" }}
           />
+          {i === arr.length -1 && <IconButton aria-label="add-year" onClick={removeYearHandler}>
+        <RemoveCircleOutlineIcon />
+      </IconButton>}
         </Stack>
       ))}
       <IconButton aria-label="add-year" onClick={addYearHandler}>
