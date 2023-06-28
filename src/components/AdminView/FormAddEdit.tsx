63cbc4866f8e5937e130d968b7e07883ec96248c
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-06-28 14:07:28 +0200

    successfully added  addYearHandler function logic

--- src/components/AdminView/FormAddEdit.tsx
+++ src/components/AdminView/FormAddEdit.tsx
@@ -6,27 +6,32 @@ import { useState } from "react";
 export default function FormAddEdit() {
   const [yearsArr, setYearsArr] = useState<number[]>([YEARS[0]]);
 
-  const addYearHandler = () => {setYearsArr((prev => {console.log(prev[-1]); ; return [...prev, prev[-1] + 1]}))};
+  const addYearHandler = () => {
+    setYearsArr((prev) => [...prev, prev.at(-1)! + 1]);
+  };
 
+  console.log(yearsArr);
   return (
     <form method="POST">
       <Typography>Product Name</Typography>
       <TextField type="text" autoFocus />
 
       <Typography>Prices</Typography>
-      {yearsArr.map((el, i) =>     <Stack key={i} direction={"row"}>
-      <TextField
-        label="Year"
-        type="number"
-        defaultValue={String(el + i)}
-        placeholder=""
-      />
-      <TextField
-        label="Price"
-        type="number"
-        InputProps={{ endAdornment: "PLN" }}
-      />
-    </Stack>)}
+      {yearsArr.map((el, i) => (
+        <Stack key={i} direction={"row"}>
+          <TextField
+            label="Year"
+            type="number"
+            defaultValue={String(el)}
+            placeholder=""
+          />
+          <TextField
+            label="Price"
+            type="number"
+            InputProps={{ endAdornment: "PLN" }}
+          />
+        </Stack>
+      ))}
       <IconButton aria-label="add-year" onClick={addYearHandler}>
         <ControlPointIcon />
       </IconButton>
