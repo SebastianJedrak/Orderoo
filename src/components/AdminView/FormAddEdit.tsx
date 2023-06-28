0588986d8deb12e29de2c03749934dca8716ccc9
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-06-28 15:12:54 +0200

    added combobox to FormAddEdit

--- src/components/AdminView/FormAddEdit.tsx
+++ src/components/AdminView/FormAddEdit.tsx
@@ -1,10 +1,25 @@
-import { Box, IconButton, Stack, TextField, Typography } from "@mui/material";
+import {
+  Autocomplete,
+  Box,
+  FormControl,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  Select,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { YEARS } from "../ClientView/Order/SelectYear";
 import ControlPointIcon from "@mui/icons-material/ControlPoint";
 import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { ProductsContext } from "../../ctx/ProductsContext";
 
 export default function FormAddEdit() {
+  const data = useContext(ProductsContext);
+  const productItems = data!.productInSelectedYear;
+
   const [yearsArr, setYearsArr] = useState<number[]>([YEARS[0]]);
 
   const addYearHandler = () => {
@@ -14,7 +29,7 @@ export default function FormAddEdit() {
   const removeYearHandler = () => {
     setYearsArr((prev) => prev.slice(0, -1));
   };
-  
+
   return (
     <form method="POST">
       <Typography>Product Name</Typography>
@@ -42,12 +57,22 @@ export default function FormAddEdit() {
             >
               <RemoveCircleOutlineIcon />
             </IconButton>
-          ) : <Box sx={{ width: "40px", aspectRatio: 1 / 1 }}/>}
+          ) : (
+            <Box sx={{ width: "40px", aspectRatio: 1 / 1 }} />
+          )}
         </Stack>
       ))}
       <IconButton aria-label="add-year" onClick={addYearHandler}>
         <ControlPointIcon />
       </IconButton>
+
+      <Typography>Required Products</Typography>
+      <Autocomplete
+        multiple
+        id="reqProducts"
+        options={productItems!.map((product) => product.productName)}
+        renderInput={(params) => <TextField {...params}  />}
+      />
     </form>
   );
 }
