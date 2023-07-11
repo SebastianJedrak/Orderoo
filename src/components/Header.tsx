10f31ade3b9b2ebcb140b2f75e22cce865c9538a
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-07-11 10:34:43 +0200

    refactored change to admin user Button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
-import { Typography, Button, Box, Stack } from "@mui/material";
+import { Typography, Button, Box, Stack, IconButton } from "@mui/material";
 import { SetStateAction } from "react";
+import CachedIcon from '@mui/icons-material/Cached';
 
 type Props = {
   view: string;
@@ -21,18 +22,18 @@ function Header(props: Props) {
       bgcolor="primary.main"
       px={3}
     >
-      <Box sx={{ width: "11%" }} />
+      <Box sx={{ width: "100px" }} />
       <Typography py={2} textAlign="center" color="common.white" variant="h4">
         Media Connect
       </Typography>
       <Button
         onClick={changeViewHandler}
-        sx={{ width: "11%" }}
+        sx={{ width: "100px" }}
         size="small"
         variant="contained"
         color="secondary"
       >
-        Change to {props.view === "admin" ? "user" : "admin"}
+       <CachedIcon sx={{ mr:0.5 }} /> {props.view === "admin" ? "user" : "admin"}
       </Button>
     </Stack>
   );
